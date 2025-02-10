1f7f88b0cc579f5906f546af72e8f12602946095
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-02-09 19:29:19 -0800

    Update flags.py Removed UOV Strong Variation
    Let the slider do it.
    
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- modules/flags.py
+++ modules/flags.py
@@ -4,12 +4,11 @@ from enum import IntEnum, Enum
 disabled = 'Disabled'
 enabled = 'Enabled'
 subtle_variation = 'Vary'
-strong_variation = 'Describe/Vary'
 upscale_15 = 'Upscale (1.5x)'
 upscale_2 = 'Upscale (2x)'
 upscale_fast = 'Upscale (Fast 2x)'
 
-uov_list = [disabled, subtle_variation, strong_variation, upscale_15, upscale_2, upscale_fast]
+uov_list = [disabled, subtle_variation, upscale_15, upscale_2, upscale_fast]
 
 enhancement_uov_before = "Before First Enhancement"
 enhancement_uov_after = "After Last Enhancement"
