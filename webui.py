c3c3fd29a15d372d046327318b1706baf64deffb
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-02-09 18:46:38 -0800

    Update webui.py Remove trigger_auto_describe from uov_input_image.upload()
    Restore default mainline Fooocus UOV operation
    
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- webui.py
+++ webui.py
@@ -1370,7 +1370,7 @@ with common.GRADIO_ROOT:
                     return trigger_describe(mode, img, apply_styles)
                 return gr.update(), gr.update()
 
-            uov_input_image.upload(trigger_auto_describe, inputs=[describe_methods, uov_input_image, prompt, describe_apply_styles],
+            uov_input_image.upload(inputs=[describe_methods, uov_input_image, prompt, describe_apply_styles],
                                    outputs=[prompt, style_selections], show_progress=True, queue=True) \
                 .then(fn=style_sorter.sort_styles, inputs=style_selections, outputs=style_selections, queue=False, show_progress=False) \
                 .then(lambda: None, _js='()=>{refresh_style_localization();}')
