2789adb09bb1befa60d8484fcbc9fae5bcd83e65
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-02-11 15:34:22 -0800

    Update shared.py Removed the blank line before "initializing..." msg
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- custom/shared.py
+++ custom/shared.py
@@ -20,7 +20,5 @@ def init_module(file_path):
     module_root = os.path.dirname(file_path)
     sys.path.append(module_root)
     module_name = os.path.relpath(module_root, os.path.dirname(os.path.abspath(__file__)))
-    print()
     print(f'[{module_name}] The custom module: {module_name} is initializing...')
     return module_name, module_root
-
