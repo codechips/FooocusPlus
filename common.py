ad0c44c937831bfbe934770b8615e298693f6a24
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-02-11 20:33:13 -0800

    Update common.py Removed the toggle function
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- common.py
+++ common.py
@@ -9,16 +9,3 @@ torch_device = ''
 # ROOT is used as a constant that
 # is referenced by several modules
 ROOT = os.path.dirname(os.path.abspath(__file__))
-
-# Toggle any variable, with optional console msg
-def toggle(var_name,var_string):
-  var_name = not var_name
-  if var_name == True:
-    bool_string = 'Enabled'
-  else:
-    bool_string = 'Disabled'    
-  try:
-    print(f'{var_string} is {bool_string}')
-  except:
-    pass
-  return var_name
