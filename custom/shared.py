797f86aaa2e7073cce9e4c96fe4f2a3982d7852e
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-02-12 11:17:39 -0800

    Update shared.py Improved msg formatting
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- custom/shared.py
+++ custom/shared.py
@@ -20,5 +20,5 @@ def init_module(file_path):
     module_root = os.path.dirname(file_path)
     sys.path.append(module_root)
     module_name = os.path.relpath(module_root, os.path.dirname(os.path.abspath(__file__)))
-    print(f'[{module_name}] The custom module: {module_name} is initializing...')
+    print(f'[{module_name}] The {module_name} custom module is initializing...')
     return module_name, module_root
