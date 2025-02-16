90ddc5163b5c206779afa20d90bb2b67a64baf9f
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-02-16 13:28:31 -0800

    Initialized new module, welcome.py
    Currently holds get_welcome_image() but later it will hold dynamic image processor
    
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

---
+++ enhanced/welcome.py
@@ -0,0 +1,29 @@
+import os
+import glob
+import modules.util as util
+
+
+def get_welcome_image():
+    path_welcome = os.path.abspath(f'./enhanced/welcome_images/')
+    skip_jpg = os.path.join(path_welcome, 'skip.jpg')
+    if not os.path.isfile(skip_jpg):                   # if skip.jpg exists then ignore all jpgs & jpegs
+        image_count = len(glob.glob1(path_welcome,'*.jpg')) + len(glob.glob1(path_welcome,'*.jpeg'))
+        if image_count > 0:
+            welcomes = [p for p in util.get_files_from_folder(path_welcome, ['.jpg', '.jpeg'], None, None)]
+            return os.path.join(path_welcome, random.choice(welcomes))
+
+    skip_png = os.path.join(path_welcome, 'skip.png')
+    if not os.path.isfile(skip_png):                   # if skip.png exists then use the fallback, welcome.png
+        image_count = len(glob.glob1(path_welcome,'*.png'))
+        if image_count > 1:
+            welcomes = [p for p in util.get_files_from_folder(path_welcome, '.png', None, None) if p != 'welcome.png']
+            if len(welcomes) > 0:
+                file_welcome = random.choice(welcomes) # a call to the dynamic startup code will follow this line
+                return os.path.join(path_welcome, file_welcome)
+    file_welcome = os.path.join(path_welcome, 'welcome.png')
+    if not os.path.isfile(file_welcome):
+        print()
+        print(f'SERIOUS ERROR: PLEASE RESTORE {file_welcome}')
+        print()
+    return file_welcome
+
