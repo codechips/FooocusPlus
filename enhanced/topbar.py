9fcc74dd75aeb61c1223e7240c09f0e7897e99cf
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-02-16 13:27:00 -0800

    Update topbar.py Moving get_welcome_image() to its own module
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- enhanced/topbar.py
+++ enhanced/topbar.py
@@ -1,5 +1,4 @@
 import os
-import glob
 import json
 import gradio as gr
 import modules.util as util
@@ -22,6 +21,7 @@ import enhanced.superprompter as superprompter
 import enhanced.comfy_task as comfy_task
 from args_manager import args
 from enhanced.simpleai import comfyd
+from enhanced.welcome import get_welcome_image
 from modules.model_loader import load_file_from_url
 
 # hard-coded limit to topbar preset display
@@ -42,31 +42,6 @@ if os.path.exists(enhanced_config):
         config_ext.update(json.load(json_file))
 else:
     config_ext.update({'fooocus_line': '# 2.1.852', 'simplesdxl_line': '# 2023-12-20'})
-
-
-def get_welcome_image():
-    path_welcome = os.path.abspath(f'./enhanced/welcome_images/')      
-    skip_jpg = os.path.join(path_welcome, 'skip.jpg')
-    if not os.path.isfile(skip_jpg):                   # if skip.jpg exists then ignore all jpgs & jpegs
-        image_count = len(glob.glob1(path_welcome,'*.jpg')) + len(glob.glob1(path_welcome,'*.jpeg'))
-        if image_count > 0:
-            welcomes = [p for p in util.get_files_from_folder(path_welcome, ['.jpg', '.jpeg'], None, None)]
-            return os.path.join(path_welcome, random.choice(welcomes))
-            
-    skip_png = os.path.join(path_welcome, 'skip.png')
-    if not os.path.isfile(skip_png):                   # if skip.png exists then use the fallback, welcome.png    
-        image_count = len(glob.glob1(path_welcome,'*.png'))
-        if image_count > 1:
-            welcomes = [p for p in util.get_files_from_folder(path_welcome, '.png', None, None) if p != 'welcome.png']
-            if len(welcomes) > 0:
-                file_welcome = random.choice(welcomes) # a call to the dynamic startup code will follow this line
-                return os.path.join(path_welcome, file_welcome)
-    file_welcome = os.path.join(path_welcome, 'welcome.png')
-    if not os.path.isfile(file_welcome):
-        print()
-        print(f'SERIOUS ERROR: PLEASE RESTORE {file_welcome}')
-        print()
-    return file_welcome
    
 
 def get_preset_name_list():
