b13160e55638e82fda0f9169c54cc9ce4d3db661
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-02-15 16:42:12 -0800

    Update topbar.py Repaired errors with control image specs.
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- enhanced/topbar.py
+++ enhanced/topbar.py
@@ -47,14 +47,14 @@ else:
 def get_welcome_image():
     path_welcome = os.path.abspath(f'./enhanced/attached/')      
     skip_jpg = os.path.join(path_welcome, 'skip.jpg')
-    if not os.path.isfile(skip_jpg):                  # if skip.jpg exists then ignore all jpgs & jpegs
+    if not os.path.isfile(skip.jpg):                   # if skip.jpg exists then ignore all jpgs & jpegs
         image_count = len(glob.glob1(path_welcome,'*.jpg')) + len(glob.glob1(path_welcome,'*.jpeg'))
         if image_count > 0:
             welcomes = [p for p in util.get_files_from_folder(path_welcome, ['.jpg', '.jpeg'], None, None)]
             return os.path.join(path_welcome, random.choice(welcomes))
             
     skip_png = os.path.join(path_welcome, 'skip.png')
-    if not os.path.isfile(skip_png):                  # if skip.png exists then use the fallback, welcome.png    
+    if not os.path.isfile(skip.png):                   # if skip.png exists then use the fallback, welcome.png    
         image_count = len(glob.glob1(path_welcome,'*.png'))
         if image_count > 0:
             welcomes = [p for p in util.get_files_from_folder(path_welcome, ['.png'], None, None) if p != 'welcome.png']
