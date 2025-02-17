7763d279ee44fe8ad27b62ce1375d08ef9664c00
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-02-16 20:20:30 -0800

    Update welcome.py Initialized dynamic startup image generation
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- enhanced/welcome.py
+++ enhanced/welcome.py
@@ -1,7 +1,11 @@
 import os
 import glob
 import modules.util as util
+import numpy as np
 import random
+import time
+from pathlib import Path
+from PIL import Image
 
 
 def get_welcome_image():
@@ -20,6 +24,7 @@ def get_welcome_image():
             welcomes = [p for p in util.get_files_from_folder(path_welcome, '.png', None, None) if p != 'welcome.png']
             if len(welcomes) > 0:
                 file_welcome = random.choice(welcomes) # a call to the dynamic startup code will follow this line
+                
                 return os.path.join(path_welcome, file_welcome)
     file_welcome = os.path.join(path_welcome, 'welcome.png')
     if not os.path.isfile(file_welcome):
@@ -28,3 +33,133 @@ def get_welcome_image():
         print()
     return file_welcome
 
+
+# color database
+def get_colors():
+    rgb_values = (
+        [0,0,0],
+        [4,47,128],
+        [14,54,96],
+        [40,40,40], 
+        [52,152,219],
+        [69,69,69],
+        [114,114,114],
+        [153,153,153],
+        [178,178,178],
+        [184,215,249],
+        [222,220,230],
+        [251,237,199],
+        [238,238,238],
+        [255,255,255],
+    )
+    num_colors = len(rgb_values) - 1
+    (randA, randB) = random.sample(range(0, num_colors), 2)
+    colorA = rgb_values[randA]
+    colorB = rgb_values[randB]
+    return colorA, colorB
+
+
+def generate_background(width, height):
+    (colorA, colorB) = get_colors()
+
+    # we always generate square to avoid tensor errors
+    # size increases to improve output quality on resize
+    size_width = width * 2
+    size_height = size_width
+    axis = size_width
+
+    gradient = np.zeros((size_height, size_width, 3), np.uint8)
+    randomizer = random.sample(range(0, 100), 1)
+    if (randomizer[0] % 2) == 0:
+        axis = size_height
+    else:
+        axis = size_width
+    
+    # Fill R, G and B channels with linear gradient between two end colours
+    gradient[:,:,0] = np.linspace(colorA[0], colorB[0], axis, dtype=np.uint8)
+    gradient[:,:,1] = np.linspace(colorA[1], colorB[1], axis, dtype=np.uint8)
+    gradient[:,:,2] = np.linspace(colorA[2], colorB[2], axis, dtype=np.uint8)
+    background_rgb = Image.fromarray(gradient, 'RGB')
+
+    # different gradient direction on some generations
+    randomizer = random.sample(range(0, 100), 1)
+    if (randomizer[0] % 2) == 0:
+        background_rgb = background_rgb.rotate(90)
+
+    # resize to requested resolution
+    background_rgb = background_rgb.resize((width, height), Image.LANCZOS)
+    return background_rgb
+
+# pil image paste
+def add_logo(background, logo):
+    logo_file = Path(logo).resolve()
+    logo_rgb = Image.open(logo_file).convert("RGBA")
+
+    composite = background.convert("RGBA")
+    bg_width, bg_height = composite.size
+    logo_width, logo_height = logo_rgb.size
+
+    width_max = bg_width / 3
+    height_max = bg_height / 2.25
+
+    if bg_height > bg_width:
+        test_width = bg_height
+        test_max = height_max
+    else:
+        test_width = bg_width
+        test_max = width_max
+
+    if test_width > test_max:
+        resize_scale = logo_width / test_max
+        resize_width = int(logo_width / resize_scale)
+        resize_height = int(logo_height / resize_scale)
+        logo_rgb = logo_rgb.resize((resize_width, resize_height), Image.LANCZOS)
+        logo_width, logo_height = logo_rgb.size
+
+    if bg_height > bg_width:
+        # put the logo in the centre
+        margin_horizontal = int((bg_width - logo_width) / 2)
+        margin_vertical = int(height_max / 5)
+    else:
+        # put the logo on the right hand side
+        margin_horizontal = int(width_max / 10)
+        margin_vertical = int(height_max / 10)
+
+    paste_horizontal = bg_width - logo_width - margin_horizontal
+    paste_vertical = bg_height - logo_height - margin_vertical
+
+    composite.paste(logo_rgb, (paste_horizontal, paste_vertical), mask=logo_rgb)
+    return composite
+
+def splashscreen(width, height, logo):
+    splash_bg = generate_background(width, height)
+    splash_full = add_logo(splash_bg, logo)
+    return splash_full
+
+def test_splash(width, height):
+    test_image = splashscreen(width, height, 'logo.png')
+    test_timestamp = time.strftime("%Y%m%d-%H%M%S")
+    test_filename = (test_timestamp + '_' + str(width) + '_' + str(height) + '.png')
+    test_image.save(test_filename, format="png", quality=100)
+
+def test_desktop_splash():
+    test_splash(1344, 752)
+
+from enhanced import splashimage
+
+def test_welcome_image(is_mobile=False):
+    # define the size
+    width = 1152
+    height = 896
+    # get the logo file ready
+    path_logo = os.path.join(root, 'logo.png') # need to put the logo png someplace
+    # generate the temporary filename
+    splash_timestamp = time.strftime("%Y%m%d-%H%M%S")
+    splash_filename = (splash_timestamp + '_' + str(width) + '_' + str(height) + '.png')
+    path_splash = os.path.join(args.temp_path, splash_filename)
+    # generate and save the temporary file
+    splash_image = splashscreen(width, height, path_logo)
+    splash_image.save(path_splash, format="png", quality=100)
+    # return the path to the ui
+    return path_splash
+    
