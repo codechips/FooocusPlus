5f6054df1fdf656bed6d697112a3e7d999a64565
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-02-20 15:19:22 -0800

    Update utils.py Removed def load_file_from_url() which is an exact duplicate of modules.models_loader
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- simpleai_base/utils.py
+++ simpleai_base/utils.py
@@ -62,30 +62,6 @@ def get_size_subfolders(folder_path):
             total_size += os.path.getsize(full_path)
     return total_size
 
-def load_file_from_url(
-        url: str,
-        *,
-        model_dir: str,
-        progress: bool = True,
-        file_name: Optional[str] = None,
-) -> str:
-    """Download a file from `url` into `model_dir`, using the file present if possible.
-
-    Returns the path to the downloaded file.
-    """
-    domain = os.environ.get("HF_MIRROR", "https://huggingface.co").rstrip('/')
-    url = str.replace(url, "https://huggingface.co", domain, 1)
-    os.makedirs(model_dir, exist_ok=True)
-    if not file_name:
-        parts = urlparse(url)
-        file_name = os.path.basename(parts.path)
-    cached_file = os.path.abspath(os.path.join(model_dir, file_name))
-    if not os.path.exists(cached_file):
-        print(f'Downloading: "{url}" to {cached_file}\n')
-        from torch.hub import download_url_to_file
-        download_url_to_file(url, cached_file, progress=progress)
-    return cached_file
-
 def load_model_for_path(models_url, root_name):
     models_root = folder_paths.get_folder_paths(root_name)[0]
     for model_path in models_url:
