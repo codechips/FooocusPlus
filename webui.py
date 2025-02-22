e4405590f6ac4eb10b20ba7fcf54d671d25a015f
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-02-22 11:19:34 -0800

    Update webui.py Test print of video_sys
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- webui.py
+++ webui.py
@@ -1073,9 +1073,12 @@ with common.GRADIO_ROOT:
                 with gr.Row():
                     try:
                         video_sys = get_torch_device_name(get_torch_device())
+                        print()
+                        print(f'{video_sys}')
+                        print()                        
                         #common.torch_device = "{}".format(get_torch_device())
                     except:
-                        video_sys = 'Could not determine the video system.'
+                        video_sys = 'Unknown (could not determine the video system)'
                     if args_manager.args.always_offload_from_vram smartmem = 'Disabled' else smartmem = 'Enabled'                        
                     gr.Markdown(value=f'<h3>System Information</h3>\
                     System RAM: {int(ldm_patched.modules.model_management.get_sysram())} MB,\
