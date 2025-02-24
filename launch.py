563be0acd8cf666db20c9d177310d530522a7de2
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-02-24 12:06:28 -0800

    Update launch.py Call "get_required_library()" not "required_library()"
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- launch.py
+++ launch.py
@@ -29,7 +29,7 @@ from modules.model_loader import load_file_from_url
 REINSTALL_ALL = False
 TRY_INSTALL_XFORMERS = False
 
-if not version.required_library():
+if not version.get_required_library():
     print()
     print('Our apologies for the inconvenience, but the installed')
     print('Python library does not support Fooocus {fooocus_version.version}')
