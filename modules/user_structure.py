67bb1ce2b06343d2cddb498130a8ee91c8e4810c
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-02-28 14:19:11 -0800

    Update user_structure.py
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- modules/user_structure.py
+++ modules/user_structure.py
@@ -23,9 +23,9 @@ def create_model_structure():
 def create_user_structure():  
   create_model_structure()
   # initialize the user directory, user_dir
-  shutil.copytree('./master_batch_startups', os.path.join(args_manager.args.user_dir, '/batch_startups'), dirs_exist_ok = True)
-  shutil.copytree('./master_control_images', os.path.join(args_manager.args.user_dir, '/control_images'), dirs_exist_ok = True)
-  shutil.copytree('./master_presets', os.path.join(args_manager.args.user_dir, '/presets'), dirs_exist_ok = True)
-  shutil.copytree('./master_welcome_images', os.path.join(args_manager.args.user_dir, '/welcome_images'), dirs_exist_ok = True)
-  shutil.copytree('./master_wildcards', os.path.join(args_manager.args.user_dir, '/wildcards'), dirs_exist_ok = True)
+  shutil.copytree('./master_batch_startups', (os.path.join(args_manager.args.user_dir, '/batch_startups')), dirs_exist_ok = True)
+  shutil.copytree('./master_control_images', (os.path.join(args_manager.args.user_dir, '/control_images')), dirs_exist_ok = True)
+  shutil.copytree('./master_presets', (os.path.join(args_manager.args.user_dir, '/presets')), dirs_exist_ok = True)
+  shutil.copytree('./master_welcome_images', (os.path.join(args_manager.args.user_dir, '/welcome_images')), dirs_exist_ok = True)
+  shutil.copytree('./master_wildcards', (os.path.join(args_manager.args.user_dir, '/wildcards')), dirs_exist_ok = True)
   return
