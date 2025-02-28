35e3fc3ece044bb15c9ffcc8a9e1f1455855a57c
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-02-27 22:26:55 -0800

    Update model_structure.py Added the rest of the master folders, renamed to create_user_structure()
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- modules/model_structure.py
+++ modules/model_structure.py
@@ -3,7 +3,7 @@ import shutil
 import args_manager
 from modules import config
 
-def create_model_structure():
+def create_user_structure():
   # ensure that all the special model directories exist
   os.makedirs(config.paths_checkpoints[0] + '\Alternative', exist_ok=True)
   os.makedirs(config.paths_checkpoints[0] + '\Flux', exist_ok=True)
@@ -20,5 +20,8 @@ def create_model_structure():
   os.makedirs(config.paths_loras[0] + '\SD3x', exist_ok=True)
 
   shutil.copytree('./master_batch_startups', f'{args_manager.args.user_dir}/batch_startups')
+  shutil.copytree('./master_control_images', f'{args_manager.args.user_dir}/control_images')
   shutil.copytree('./master_presets', f'{args_manager.args.user_dir}/presets')
+  shutil.copytree('./master_welcome_images', f'{args_manager.args.user_dir}/welcome_images')
+  shutil.copytree('./master_wildcards', f'{args_manager.args.user_dir}/wildcards')
   return
