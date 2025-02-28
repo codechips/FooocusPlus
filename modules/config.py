fd7132fe843e60c906d1838974162429cee66076
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-02-28 14:53:08 -0800

    Update config.py
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- modules/config.py
+++ modules/config.py
@@ -106,9 +106,9 @@ def try_load_deprecated_user_path_config():
 try_load_deprecated_user_path_config()
 
 def get_presets():
-    preset_folder = f'{args_manager.args.user_dir}, \\presets')
+    preset_folder = f'{args_manager.args.user_dir}, \\presets'
     print()
-    print(f'args_manager.args.user_dir: {args_manager.args.user_dir}')    
+    print(f'args_manager.args.user_dir: {args_manager.args.user_dir}')   
     print(f'Preset Folder: {preset_folder}')
     print()
     presets = ['initial']
