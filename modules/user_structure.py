1819454ce3461a86b1c674bfc62b3594516aac59
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-02-28 09:00:12 -0800

    Update and rename model_structure.py to user_structure.py
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- modules/user_structure.py
+++ modules/user_structure.py
@@ -3,7 +3,7 @@ import shutil
 import args_manager
 from modules import config
 
-def create_user_structure():
+def create_model_structure():
   # ensure that all the special model directories exist
   os.makedirs(config.paths_checkpoints[0] + '\Alternative', exist_ok=True)
   os.makedirs(config.paths_checkpoints[0] + '\Flux', exist_ok=True)
@@ -18,7 +18,10 @@ def create_user_structure():
   os.makedirs(config.paths_loras[0] + '\Pony', exist_ok=True)
   os.makedirs(config.paths_loras[0] + '\SD1.5', exist_ok=True)
   os.makedirs(config.paths_loras[0] + '\SD3x', exist_ok=True)
+  return
 
+def create_user_structure():
+  create_model_structure()
   shutil.copytree('./master_batch_startups', f'{args_manager.args.user_dir}/batch_startups')
   shutil.copytree('./master_control_images', f'{args_manager.args.user_dir}/control_images')
   shutil.copytree('./master_presets', f'{args_manager.args.user_dir}/presets')
