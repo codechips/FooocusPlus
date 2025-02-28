baa3893166f0b893cd5cb3caf97768745dcf6910
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-02-28 09:41:08 -0800

    Update user_structure.py Simply user structure destination paths
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- modules/user_structure.py
+++ modules/user_structure.py
@@ -23,9 +23,9 @@ def create_model_structure():
 def create_user_structure():  
   create_model_structure()
   # initialize the user directory, user_dir
-  shutil.copytree('./master_batch_startups', f'{args_manager.args.user_dir}/batch_startups')
-  shutil.copytree('./master_control_images', f'{args_manager.args.user_dir}/control_images')
-  shutil.copytree('./master_presets', f'{args_manager.args.user_dir}/presets')
-  shutil.copytree('./master_welcome_images', f'{args_manager.args.user_dir}/welcome_images')
-  shutil.copytree('./master_wildcards', f'{args_manager.args.user_dir}/wildcards')
+  shutil.copytree('./master_batch_startups', f'{args_manager.args.user_dir}batch_startups')
+  shutil.copytree('./master_control_images', f'{args_manager.args.user_dir}control_images')
+  shutil.copytree('./master_presets', f'{args_manager.args.user_dir}presets')
+  shutil.copytree('./master_welcome_images', f'{args_manager.args.user_dir}welcome_images')
+  shutil.copytree('./master_wildcards', f'{args_manager.args.user_dir}wildcards')
   return
