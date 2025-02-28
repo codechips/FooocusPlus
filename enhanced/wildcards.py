d6994ad81bf316626948df545c84bf0e58bd98eb
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-02-28 15:59:31 -0800

    Update wildcards.py Define args.user_dir
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- enhanced/wildcards.py
+++ enhanced/wildcards.py
@@ -7,7 +7,7 @@ import enhanced.translator as translator
 from modules.util import get_files_from_folder
 from args_manager import args
 
-wildcards_path = f'{args.args.user_dir}\\wildcards'
+wildcards_path = f'{args.user_dir}\\wildcards'
 wildcards_max_bfs_depth = 64
 wildcards = {}
 wildcards_list = {}
