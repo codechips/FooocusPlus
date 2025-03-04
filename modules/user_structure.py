dbdca255b7b94e45ba6e4b302d0509be47309b3a
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-03-04 12:26:39 -0800

    Update user_structure.py Introduced advanced preset control
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- modules/user_structure.py
+++ modules/user_structure.py
@@ -1,8 +1,6 @@
 import os
-import sys
 import shutil
 import args_manager
-import common
 from modules import config
 
 def create_model_structure():
@@ -28,13 +26,23 @@ def create_user_structure():
   # initialize the user directory, user_dir
   shutil.copytree('./master_batch_startups', f'{args_manager.args.user_dir}/batch_startups', dirs_exist_ok = True)
   shutil.copytree('./master_control_images', f'{args_manager.args.user_dir}/control_images', dirs_exist_ok = True)
-  shutil.copytree('./master_presets', f'{args_manager.args.user_dir}/presets', dirs_exist_ok = True)
   shutil.copytree('./master_welcome_images', f'{args_manager.args.user_dir}/welcome_images', dirs_exist_ok = True)
   shutil.copytree('./master_wildcards', f'{args_manager.args.user_dir}/wildcards', dirs_exist_ok = True)
-  if os.path.exists(f'{args_manager.args.user_dir}/presets'):
-    working_presets = os.path.abspath(f'{args_manager.args.user_dir}/presets')
-    sys.path.append(working_presets)
-    common.working_presets = working_presets
-    print(f'Working Presets: {working_presets}')
-    print()
+
+  # copy './master_presets' to user_dir for the user's reference only
+  # ensure that './presets' exists
+  # delete the contents of './presets' which is used as a temporary working folder
+  # initialize './presets' with the contents of './master_presets'
+  # overwrite './presets' with the contents of user_dir './user_presets'
+  # this allows a user to completely customize their presets, if desired
+  shutil.copytree('./master_presets', f'{args_manager.args.user_dir}/master_presets', dirs_exist_ok = True)
+  working_presets = './presets'
+  if os.path.exists(working_presets):
+    shutil.rmtree(working_presets, ignore_errors=True)
+  shutil.copytree('./master_presets', working_presets, dirs_exist_ok = True)
+  os.makedirs(f'{args_manager.args.user_dir}/user_presets', exist_ok = True)
+  if os.path.exists(f'{args_manager.args.user_dir}/user_presets'):
+    shutil.copytree(f'{args_manager.args.user_dir}/user_presets', working_presets, dirs_exist_ok = True)
+  print(f'Updated working preset folder: {working_presets}')
+  print()
   return
