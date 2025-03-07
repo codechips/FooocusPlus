679c7759f8330892296b9637c4fd6cd79717cc65
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-03-07 08:55:46 -0800

    Update webui.py Make help notes for "Save New Preset"
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- webui.py
+++ webui.py
@@ -1042,8 +1042,8 @@ with common.GRADIO_ROOT:
                     # selection is later restored to the UI it needs to be more universal.
                     language_ui = gr.Radio(visible=False, label='Language of UI', choices=['En', '中文'], value=modules.flags.language_radio(args_manager.args.language), interactive=False)
                     background_theme = gr.Radio(label='Background Theme', choices=['light', 'dark'], value=args_manager.args.theme, interactive=True)
-                    prompt_preset_button = gr.Button(label='Make New Preset', \
-                        value='All current parameters will be saved. Clear the positive and negative prompts unless you want them to be part of the preset')
+                    prompt_preset_button = gr.Button(value='Make New Preset')
+                    gr.Markdown(value='All current parameters will be saved. Clear the positive and negative prompts unless you want them to be part of the preset')
                 with gr.Group():
                     comfyd_active_checkbox = gr.Checkbox(label='Enable Comfyd Always Active', value=not args_manager.args.disable_comfyd, info='Enabling will improve execution speed but occupy some memory.')
                     image_tools_checkbox = gr.Checkbox(label='Enable ParamsTools', value=True, info='Management of published image sets, located in the middle toolbox on the right side of the image set.')
