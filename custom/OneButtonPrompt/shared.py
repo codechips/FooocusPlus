2d5e4ee8bdc9d84c7c28986d3731031a640979ec
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-03-11 17:18:47 -0700

    Update shared.py Removed call to RuinedFooocus PathManager
    Yes, I just discovered SimpleAI's wholesale importation of RuinedFooocus support files, along with OneButtonPrompt
    
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- custom/OneButtonPrompt/shared.py
+++ custom/OneButtonPrompt/shared.py
@@ -3,8 +3,8 @@ import sys
 import custom.shared as shared
 module_name, module_root = shared.init_module(os.path.abspath(__file__))
 
-from .modules.path import PathManager
-path_manager = PathManager()
+#from .modules.path import PathManager
+#path_manager = PathManager()
 
 state = {"preview_image": None, "ctrls_name": [], "ctrls_obj": [], "pipeline": None}
 
