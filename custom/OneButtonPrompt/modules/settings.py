2b11ad3537f539af2d44bc42073d5b2572466756
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-03-12 18:44:16 -0700

    Update settings.py Removed all DEFAULT_SETTINGS except OBP_Preset and hint_chance
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- custom/OneButtonPrompt/modules/settings.py
+++ custom/OneButtonPrompt/modules/settings.py
@@ -4,28 +4,6 @@ from custom.OneButtonPrompt.shared import path_manager
 from custom.OneButtonPrompt.utils import path_fixed
 
 DEFAULT_SETTINGS = {
-    "advanced_mode": False,
-    "image_number": 1,
-    "seed_random": True,
-    "seed": 0,
-    "style": "Style: sai-cinematic",
-    "prompt": "",
-    "negative_prompt": "",
-    "performance": "Speed",
-    "resolution": "1152x896 (4:3)",
-    "base_model": path_manager.default_model_names["default_base_model_name"],
-    "lora_1_model": path_manager.default_model_names["default_lora_name"],
-    "lora_1_weight": path_manager.default_model_names["default_lora_weight"],
-    "lora_2_model": "None",
-    "lora_2_weight": path_manager.default_model_names["default_lora_weight"],
-    "lora_3_model": "None",
-    "lora_3_weight": path_manager.default_model_names["default_lora_weight"],
-    "lora_4_model": "None",
-    "lora_4_weight": path_manager.default_model_names["default_lora_weight"],
-    "lora_5_model": "None",
-    "lora_5_weight": path_manager.default_model_names["default_lora_weight"],
-    "theme": "None",
-    "auto_negative_prompt": False,
     "OBP_preset": "Standard",
     "hint_chance": 25,
 }
