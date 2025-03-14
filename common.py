33f9576452e3656c41d52a064daf363bf42eb251
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-03-13 18:51:58 -0700

    Update common.py Make EXTRA_VARIATION pseudo-global all caps.
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- common.py
+++ common.py
@@ -2,6 +2,7 @@ import os
 
 # These pseudo globals are imported by several
 # functions and are subject to change
+EXTRA_VARIATION = False
 GRADIO_ROOT = None
 MODELS_INFO = None
 torch_device = ''
@@ -9,7 +10,3 @@ torch_device = ''
 # ROOT is used as a constant that
 # is referenced by several modules
 ROOT = os.path.dirname(os.path.abspath(__file__))
-
-# Instead of disturbing the list exchange from webui
-# to async_worker, do it the basic way
-extra_variation = False
