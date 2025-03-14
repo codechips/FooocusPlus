066db7cfedf9dbf56de6b710440107e4dc991e5e
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-03-13 17:19:15 -0700

    Update common.py Initialized extra_variation = False
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- common.py
+++ common.py
@@ -9,3 +9,7 @@ torch_device = ''
 # ROOT is used as a constant that
 # is referenced by several modules
 ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Instead of disturbing the list exchange from webui
+# to async_worker, do it the basic way
+extra_variation = False
