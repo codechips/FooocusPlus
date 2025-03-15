00df3ff96d374f49f94c454ffb08e080acd1e75d
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-03-14 21:21:47 -0700

    Update common.py Added EV_Base to support async_worker.py
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- common.py
+++ common.py
@@ -6,6 +6,10 @@ GRADIO_ROOT = None
 MODELS_INFO = None
 torch_device = ''
 
+# EV_Base is only used by the Extra Variation
+# function in async_worker.py
+EV_Base = 0
+
 # ROOT is used as a constant that
 # is referenced by several modules
 ROOT = os.path.dirname(os.path.abspath(__file__))
