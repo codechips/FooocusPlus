322553648eb32d9e9115acc92e3896b7ec9f8e75
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-03-17 18:29:41 -0700

    Update ui_onebutton.py Indentation error
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- custom/OneButtonPrompt/ui_onebutton.py
+++ custom/OneButtonPrompt/ui_onebutton.py
@@ -692,7 +692,7 @@ def ui_onebutton(prompt, run_event, random_button):
                     )
                     add_ctrl("obp_antistring", antistring)
 '''
-#        with gr.Row():
+        with gr.Row():
         promptenhance = gr.Dropdown(
             choices=promptenhancelist, label="HYPERPROMPTING", value="hyperprompt, visible=False"
         )
@@ -777,7 +777,7 @@ def ui_onebutton(prompt, run_event, random_button):
                     inputs=obp_outputs,
                     outputs=[OBP_preset],
                 )
-        
+   '''     
      
         def obppreset_changed(selection):
                 if selection == OBPresets.CUSTOM_OBP:
