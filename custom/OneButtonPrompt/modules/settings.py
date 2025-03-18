d931c7582821b9ce03561e1b76b4c6f4d062f196
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-03-17 17:26:18 -0700

    Update settings.py Cleanup unused code
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- custom/OneButtonPrompt/modules/settings.py
+++ custom/OneButtonPrompt/modules/settings.py
@@ -1,6 +1,5 @@
 import json
 from os.path import exists
-#from custom.OneButtonPrompt.shared import path_manager
 from custom.OneButtonPrompt.utils import path_fixed
 
 DEFAULT_SETTINGS = {
@@ -8,25 +7,14 @@ DEFAULT_SETTINGS = {
     "hint_chance": 25,
 }
 
-
 def load_settings():
-#    if exists(path_fixed("settings/settings.json")):
-#        with open(path_fixed("settings/settings.json")) as f:
-#            settings = json.load(f)
-#    else:
     settings = {}
-
     # Add any missing default settings
     changed = False
     for key, value in DEFAULT_SETTINGS.items():
         if key not in settings:
             settings[key] = value
             changed = True
-
-#    if changed:
-#        with open(path_fixed("settings/settings.json"), "w") as f:
-#            json.dump(settings, f, indent=2)
-
     return settings
 
 default_settings = load_settings()
