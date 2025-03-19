0496cc06c28b7fac9abf38582c2ee899cdc0de15
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-03-19 11:52:42 -0700

    Update user_structure.py Began to integrate create_topic_structure() into create_user_structure()
    Also improved the logic and documentation for create_topic_structure()
    
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- modules/user_structure.py
+++ modules/user_structure.py
@@ -20,18 +20,8 @@ def create_model_structure():
   os.makedirs(config.paths_loras[0] + '\SD3x', exist_ok=True)
   return
 
-def create_topic_structure():
-  # initialize the One Button Prompt (Random Prompt) structure
-  shutil.copytree('./master_topics', f'{args_manager.args.user_dir}/master_topics', dirs_exist_ok = True)
-  shutil.copytree('./master_topics', './custom/OneButtonPrompt/random_prompt/userfiles', dirs_exist_ok = True)
-  os.makedirs(f'{args_manager.args.user_dir}/user_topics', exist_ok = True)
-  if os.path.exists(f'{args_manager.args.user_dir}/user_topics'):
-    shutil.copytree(f'{args_manager.args.user_dir}/user_topics',\
-      './custom/OneButtonPrompt/random_prompt/userfiles', dirs_exist_ok = True)
-  return
-
 def create_user_structure():
-  # initialize the user directory, user_dir
+  # initialize the user directory, user_dir  
   print(f'Initialized the user folder at {os.path.abspath(args_manager.args.user_dir)}')
   shutil.copytree('./master_batch_startups', f'{args_manager.args.user_dir}/batch_startups', dirs_exist_ok = True)
   shutil.copytree('./master_control_images', f'{args_manager.args.user_dir}/control_images', dirs_exist_ok = True)
@@ -40,21 +30,37 @@ def create_user_structure():
 
   # delete the contents of user_dir/master_presets to get a clean start
   # copy the contents of './master_presets' to user_dir for the user's reference only
-  # ensure that './presets' exists
-  # delete the contents of './presets' which is used as a temporary working folder
-  # initialize './presets' with the contents of './master_presets'
-  # overwrite './presets' with the contents of user_dir './user_presets'
-  # this allows a user to completely customize the master presets, if desired
   master_presets = f'{args_manager.args.user_dir}/master_presets'
   if os.path.exists(master_presets):
     shutil.rmtree(master_presets, ignore_errors=True)
-  shutil.copytree('./master_presets', f'{args_manager.args.user_dir}/master_presets', dirs_exist_ok = True)
+  shutil.copytree('./master_presets', master_presets', dirs_exist_ok = True)
+
+  # ensure that './presets' exists
+  # delete the contents of './presets' which is used as a temporary working folder
+  # initialize './presets' with the contents of './master_presets'
   working_presets = './presets'
   if os.path.exists(working_presets):
     shutil.rmtree(working_presets, ignore_errors=True)
   shutil.copytree('./master_presets', working_presets, dirs_exist_ok = True)
-  os.makedirs(f'{args_manager.args.user_dir}/user_presets', exist_ok = True)
-  if os.path.exists(f'{args_manager.args.user_dir}/user_presets'):
-    shutil.copytree(f'{args_manager.args.user_dir}/user_presets', working_presets, dirs_exist_ok = True)
-  print(f'Updated the working preset folder: {os.path.abspath(working_presets)}')
+
+  # overwrite './presets' with the contents of user_dir './user_presets'
+  # this allows a user to completely customize the master presets, if desired
+  user_presets = f'{args_manager.args.user_dir}/user_presets'
+  os.makedirs(user_presets, exist_ok = True)
+  if os.path.exists(user_presets):
+    shutil.copytree(user_presets, working_presets, dirs_exist_ok = True)
+  print(f'Updated the working preset folder: {working_presets}')
+
+  # in a similar way, initialize the One Button Prompt (Random Prompt) structure
+  master_topics = f'{args_manager.args.user_dir}/master_topics'
+  if os.path.exists(master_topics):
+    shutil.rmtree(master_topics, ignore_errors=True)
+  shutil.copytree('./master_topics', f'{args_manager.args.user_dir}/master_topics', dirs_exist_ok = True)
+  
+  working_topics = './custom/OneButtonPrompt/random_prompt/userfiles'
+  shutil.copytree('./master_topics', working_topics, dirs_exist_ok = True)
+  os.makedirs(f'{args_manager.args.user_dir}/user_topics', exist_ok = True)
+  if os.path.exists(f'{args_manager.args.user_dir}/user_topics'):
+    shutil.copytree(f'{args_manager.args.user_dir}/user_topics', working_topics, dirs_exist_ok = True)    
+  print(f'Updated the working Random Prompt topics folder: {os.path.abspath(working_presets)}')
   return
