bf86e5c4355ec8994773b762c628f0b3124081bc
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-03-19 18:41:17 -0700

    Update user_structure.pyUpdate user_structure.py Simplified the updated topics msg
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- modules/user_structure.py
+++ modules/user_structure.py
@@ -50,7 +50,7 @@ def create_user_structure():
   os.makedirs(user_topics, exist_ok = True)
   if os.path.exists(user_topics):
     shutil.copytree(user_topics, working_topics, dirs_exist_ok = True)
-  print(f'Updated the working Random Prompt topics folder: {working_topics}')
+  print(f'Updated the working Random Prompt topics folder') # : {working_topics}
 
   # in a similar way, initialize the Presets structure
   master_presets = f'{args_manager.args.user_dir}/master_presets'
