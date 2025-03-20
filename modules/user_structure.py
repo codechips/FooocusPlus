f5b5c712819bcab4e37510bbee76320a71362ada
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-03-19 18:28:42 -0700

    Update user_structure.py Initialize Topics before initializing Presets
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- modules/user_structure.py
+++ modules/user_structure.py
@@ -28,44 +28,44 @@ def create_user_structure():
   shutil.copytree('./master_welcome_images', f'{args_manager.args.user_dir}/welcome_images', dirs_exist_ok = True)
   shutil.copytree('./master_wildcards', f'{args_manager.args.user_dir}/wildcards', dirs_exist_ok = True)
 
-  # delete the contents of user_dir/master_presets to get a clean start
-  # copy the contents of './master_presets' to user_dir for the user's reference only
-  master_presets = f'{args_manager.args.user_dir}/master_presets'
-  if os.path.exists(master_presets):
-    shutil.rmtree(master_presets, ignore_errors=True)
-  shutil.copytree('./master_presets', master_presets', dirs_exist_ok = True)
-
-  # ensure that './presets' exists
-  # delete the contents of './presets' which is used as a temporary working folder
-  # initialize './presets' with the contents of './master_presets'
-  working_presets = './presets'
-  if os.path.exists(working_presets):
-    shutil.rmtree(working_presets, ignore_errors=True)
-  shutil.copytree('./master_presets', working_presets, dirs_exist_ok = True)
-
-  # overwrite './presets' with the contents of user_dir './user_presets'
-  # this allows a user to completely customize the master presets, if desired
-  user_presets = f'{args_manager.args.user_dir}/user_presets'
-  os.makedirs(user_presets, exist_ok = True)
-  if os.path.exists(user_presets):
-    shutil.copytree(user_presets, working_presets, dirs_exist_ok = True)
-  print(f'Updated the working preset folder: {working_presets}')
-
-  # in a similar way, initialize the Random Prompt (One Button Prompt) structure
+  # delete the contents of user_dir/master_topics to get a clean start
+  # copy the contents of './master_topics' to user_dir for the user's reference only
   master_topics = f'{args_manager.args.user_dir}/master_topics'
   if os.path.exists(master_topics):
     shutil.rmtree(master_topics, ignore_errors=True)
   shutil.copytree('./master_topics', f'{args_manager.args.user_dir}/master_topics', dirs_exist_ok = True)
   
+  # ensure that './custom/OneButtonPrompt/random_prompt/userfiles' exists
+  # delete the contents of 'usefiles' which is used as a temporary working folder
+  # initialize 'userfiles' with the contents of './master_topics'
   working_topics = './custom/OneButtonPrompt/random_prompt/userfiles'
   os.makedirs(working_topics, exist_ok = True)
   if os.path.exists(working_topics):
     shutil.rmtree(working_topics, ignore_errors=True)
   shutil.copytree('./master_topics', working_topics, dirs_exist_ok = True)
 
+  # overwrite 'userfiles' with the contents of user_dir './user_topics'
+  # this allows a user to completely customize the available topics, if desired
   user_topics = f'{args_manager.args.user_dir}/user_topics'
   os.makedirs(user_topics, exist_ok = True)
   if os.path.exists(user_topics):
     shutil.copytree(user_topics, working_topics, dirs_exist_ok = True)
-  print(f'Updated the working Random Prompt topics folder: {os.path.abspath(working_presets)}')
+  print(f'Updated the working Random Prompt topics folder: {working_topics}')
+
+  # in a similar way, initialize the Presets structure
+  master_presets = f'{args_manager.args.user_dir}/master_presets'
+  if os.path.exists(master_presets):
+    shutil.rmtree(master_presets, ignore_errors=True)
+  shutil.copytree('./master_presets', master_presets', dirs_exist_ok = True)
+
+  working_presets = './presets'
+  if os.path.exists(working_presets):
+    shutil.rmtree(working_presets, ignore_errors=True)
+  shutil.copytree('./master_presets', working_presets, dirs_exist_ok = True)
+
+  user_presets = f'{args_manager.args.user_dir}/user_presets'
+  os.makedirs(user_presets, exist_ok = True)
+  if os.path.exists(user_presets):
+    shutil.copytree(user_presets, working_presets, dirs_exist_ok = True)
+  print(f'Updated the working preset folder: {working_presets}')
   return
