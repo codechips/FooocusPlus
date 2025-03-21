809ef70c797f248321d43c356dad078ee6ae5135
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-03-20 20:10:09 -0700

    Update shared.py Translated "OneButtonPrompt" to "Random Prompt" in console msg
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- custom/shared.py
+++ custom/shared.py
@@ -20,5 +20,8 @@ def init_module(file_path):
     module_root = os.path.dirname(file_path)
     sys.path.append(module_root)
     module_name = os.path.relpath(module_root, os.path.dirname(os.path.abspath(__file__)))
-    print(f'[{module_name}] The {module_name} custom module is initializing...')
+    if module_name == "OneButtonPrompt":
+        print(f'[{module_name}] The Random Prompt custom module is initializing...')
+    else:
+        print(f'[{module_name}] The {module_name} custom module is initializing...')
     return module_name, module_root
