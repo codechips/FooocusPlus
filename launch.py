dae08bdeec6b6e6cecd8ca777e524d4e72b42f00
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-03-26 20:23:49 -0700

    Update launch.py Use xformers_ver instead of {xformers_ver}
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- launch.py
+++ launch.py
@@ -80,7 +80,7 @@ def prepare_environment():
     print()
 #    if TRY_INSTALL_XFORMERS:
     if REINSTALL_ALL or not is_installed("xformers"):
-        xformers_package = os.environ.get('XFORMERS_PACKAGE', {xformers_ver})
+        xformers_package = os.environ.get('XFORMERS_PACKAGE', xformers_ver)
         if platform.system() == "Windows":
             if platform.python_version().startswith("3.10"):
                 run_pip(f"install -U -I --no-deps {xformers_package}", "xformers", live=True)
