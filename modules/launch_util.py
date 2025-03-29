5c0c3bea5c4a0557ce17e839a593c111b97aa047
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-03-28 22:13:04 -0700

    Update launch_util.py Give msg if installing new package
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- modules/launch_util.py
+++ modules/launch_util.py
@@ -183,6 +183,7 @@ def is_installed_version(package, version_required):
         version_installed = importlib.metadata.version(package)
     except Exception:
         print()
+        print(f'Installing the required version of {package}: {version_required}')
         return False
     if packaging.version.parse(version_required) != packaging.version.parse(version_installed):
         print()
