c755759ac718ca8b19d1d7983ba46262379bf1f1
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-03-31 17:34:09 -0700

    Update launch_support.py Syntax error
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- launch_support.py
+++ launch_support.py
@@ -168,7 +168,7 @@ def read_torch_base():
     torch_base.close()
 #    except:
 #        torch_base_ver = 'undefined'
-return torch_base_ver
+    return torch_base_ver
 
 def write_torch_base(torch_base_ver):
     torch_base_path = os.path.abspath(f'{args_manager.args.user_dir}/torch_base.txt')
@@ -176,4 +176,3 @@ def write_torch_base(torch_base_ver):
     torch_base.write(f"Torch base version = '{torch_base_ver}'")
     torch_base.close()
     return
-
