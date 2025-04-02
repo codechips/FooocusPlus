6b340aec80703288c38cb304d9d04822790050e4
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-04-02 13:33:57 -0700

    Update launch.py Another try
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- launch.py
+++ launch.py
@@ -78,7 +78,7 @@ def prepare_environment():
     if REINSTALL_ALL or not is_installed("xformers"):
         if platform.python_version().startswith("3.10"):
 #            xformers_statement = (f"--no-deps 'xformers==xformers_ver'")
-            xformers_statement = ("xformers==" + xformers_ver, "--no-deps")
+            xformers_statement = "--no-deps", "xformers==" + xformers_ver
             torchruntime.install([xformers_statement])
         else:
             print("Installation of xformers is not supported in this version of Python.")
