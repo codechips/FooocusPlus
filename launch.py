053c3e492f0b7f4281b13059b26df296bf3f0d34
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-04-03 11:51:04 -0700

    Update launch.py Revised xformers_statement
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- launch.py
+++ launch.py
@@ -93,8 +93,8 @@ def prepare_environment():
 
     if REINSTALL_ALL or not is_installed("xformers"):
         if platform.python_version().startswith("3.10"):
-            xformers_statement = "xformers==" + xformers_ver
-            torchruntime.install([xformers_statement + " --no-deps"])
+            xformers_statement = f"xformers=={xformers_ver} --no-deps"
+            torchruntime.install([xformers_statement])
         else:
             print("Installation of xformers is not supported in this version of Python.")
             print(
