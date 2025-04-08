22e129079784cf1eeeacd4f73f65c1146ddbb3fc
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-04-08 11:53:14 -0700

    Update config.py Change metadata_scheme logic
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- modules/config.py
+++ modules/config.py
@@ -52,6 +52,8 @@ def get_dir_or_set_default(key, default_value, as_array=False, make_directory=Fa
 
     if v is not None:
         if not 'Outputs' in v:
+            if config_dict[key].lower == 'fooocus':
+               print('Metadata_scheme error')
             print(f'Failed to load config key: {json.dumps({key:v})} is invalid or does not exist; will use {json.dumps({key:default_value})} instead.')
     if isinstance(default_value, list):
         dp = []
@@ -251,7 +253,7 @@ def get_config_item_or_set_default(key, default_value, validator, disable_empty_
         return v
     else:
         if v is not None:
-            if config_dict[key].lower == 'fooocus':
+            if v.lower == 'fooocus':
                 default_value = MetadataScheme.SIMPLE.value
             else:
                 print(f'Failed to load config key: {json.dumps({key:v})} is invalid; will use {json.dumps({key:default_value})} instead.')
