0ba6429fbc7d3ab9cad0b29a9b2a6dc94d9ae2cc
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-04-10 13:27:13 -0700

    Update launch.py Indent error
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- launch.py
+++ launch.py
@@ -64,7 +64,8 @@ def prepare_environment():
 
     print(f"Python {sys.version}")
     print(f"Python Library {version.get_library_ver()}, Comfy version: {comfy.comfy_version.version}")
-    print(f"Torch base version: {torch_base_ver}")'''
+    print(f"Torch base version: {torch_base_ver}")
+'''
     print(f"FooocusPlus version: {version.get_fooocusplus_ver()}")
     print()
     print('Checking for required library files and loading Xformers...')
