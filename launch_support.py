c14fe79c8c034999889bb7ac72c14acae433183a
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-04-10 18:09:32 -0700

    Update launch_support.py Import win32_root from enhanced.version
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- launch_support.py
+++ launch_support.py
@@ -7,7 +7,7 @@ import args_manager
 import torchruntime
 from torchruntime.device_db import get_gpus
 from torchruntime.platform_detection import get_torch_platform
-from enhanced.version import is_win32_standalone_build
+from enhanced.version import is_win32_standalone_build, win32_root
 from modules.config import user_dir
 
 win32_cmd = '''
