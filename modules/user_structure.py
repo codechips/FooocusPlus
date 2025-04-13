e142944f20f84003642029a3ddebe8cdf8f1ebb0
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-04-13 11:23:02 -0700

    Update user_structure.py Replaced \Flux with \FluxDev & \FluxSchnell
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- modules/user_structure.py
+++ modules/user_structure.py
@@ -5,7 +5,8 @@ from modules import config
 def create_model_structure():
   # ensure that all the special model directories exist
   os.makedirs(config.paths_checkpoints[0] + '\Alternative', exist_ok=True)
-  os.makedirs(config.paths_checkpoints[0] + '\Flux', exist_ok=True)
+  os.makedirs(config.paths_checkpoints[0] + '\FluxDev', exist_ok=True)
+  os.makedirs(config.paths_checkpoints[0] + '\FluxSchnell', exist_ok=True)
   os.makedirs(config.paths_checkpoints[0] + '\LowVRAM', exist_ok=True)
   os.makedirs(config.paths_checkpoints[0] + '\Pony', exist_ok=True)
   os.makedirs(config.paths_checkpoints[0] + '\SD1.5', exist_ok=True)
