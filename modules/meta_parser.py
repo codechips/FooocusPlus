f32259a3128b95b7fa63b827fc54026c42ba3109
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-04-12 17:41:01 -0700

    Update meta_parser.py Try a different append
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- modules/meta_parser.py
+++ modules/meta_parser.py
@@ -222,8 +222,8 @@ def get_resolution(key: str, fallback: str | None, source_dict: dict, results: l
 #            print(f'CURRENT_ASPECT from common: {common.CURRENT_ASPECT}')
 #            width, height = eval(common.CURRENT_ASPECT)
             results.append(gr.update())
-            results.append(gr.update())
-            results.append(gr.update())
+            results.append(int(width))
+            results.append(int(height))
             return
         formatted = modules.config.add_ratio(f'{width}*{height}')
         engine = get_taskclass_by_fullname(source_dict.get('Backend Engine', source_dict.get('backend_engine', task_class_mapping['Fooocus']))) 
