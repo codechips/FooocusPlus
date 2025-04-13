b80c99d0dacf6c5688c4dfc36c9657cc822c986a
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-04-13 13:18:27 -0700

    Update meta_parser.py Force CURRENT_ASPECT to be a string
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- modules/meta_parser.py
+++ modules/meta_parser.py
@@ -220,7 +220,7 @@ def get_resolution(key: str, fallback: str | None, source_dict: dict, results: l
         width, height = eval(h)
         if (width == '0') or (height == '0') or (h == ''):
             print(f'CURRENT_ASPECT from common: {common.CURRENT_ASPECT}')
-            width, height = eval(common.CURRENT_ASPECT)
+            width, height = eval(str(common.CURRENT_ASPECT))
         formatted = modules.config.add_ratio(f'{width}*{height}')
         engine = get_taskclass_by_fullname(source_dict.get('Backend Engine', source_dict.get('backend_engine', task_class_mapping['Fooocus']))) 
         if 'engine' in source_dict:
