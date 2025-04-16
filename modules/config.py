ff43cecbe4ba74b824311f9b09c7739827ea6202
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-04-15 19:00:21 -0700

    Update config.py More tests
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- modules/config.py
+++ modules/config.py
@@ -371,12 +371,13 @@ default_sd1_aspect = get_config_item_or_set_default(
 #    for template, ratio in zip(AR.aspect_ratios_templates, default_standard_aspect)
 #}
 
-available_aspect_ratios_list = {
-    template: [AR.add_ratio(x) for x in ratios]
-    for template, ratios in zip(AR.aspect_ratios_templates, available_aspect_ratios)
-}
+#available_aspect_ratios_list = {
+#    template: [AR.add_ratio(x) for x in ratios]
+#    for template, ratios in zip(AR.aspect_ratios_templates, available_aspect_ratios)
+#}
+#default_aspect_ratio = AR.default_aspect_ratios['SDXL']
 
-available_aspect_ratios_labels = available_aspect_ratios_list['SDXL']
+available_aspect_ratios_labels = AR.available_aspect_ratios_list['SDXL']
 
 '''
 available_standard_aspects = get_config_item_or_set_default(
