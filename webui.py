e7ebc3ff14c7a861392fa65ed09af2fd1f6a5ce6
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-04-20 09:59:13 -0700

    Update webui.py Try bar_dropdown[i]
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- webui.py
+++ webui.py
@@ -207,12 +207,12 @@ with common.GRADIO_ROOT:
                         # disable the iFrame display of help for preset selections:
                         preset_instruction = gr.HTML(visible=False, value=topbar.preset_no_instruction())
                         
-                        bar_title = gr.Markdown('<b>Presets:  </b>', visible=True, elem_id='bar_title', elem_classes='bar_title')
+                        bar_title = gr.Markdown('<b>Presets:</b>', visible=True, elem_id='bar_title', elem_classes='bar_title')
                         bar_buttons = []
                         category_list = ['Favorites', 'Alternative', 'Fantasy', 'Flux1D', 'Flux1S', 'LowVRAM', 'Pony', 'Realism', 'SD1.5']
                         for i in range(len(category_list)):
-                            bar_buttons.append(gr.Button(value=category_list[i], size='sm', visible=True, min_width=70, elem_id=f'bar{i}', elem_classes='bar_button'))
-                        #bar_dropdown = gr.Dropdown(show_label=False, choices=['self','preset1','preset2','preset3'], value='self')
+                            bar_buttons.append(gr.Button(value=category_list[i], size='sm', visible=True, min_width=50, elem_id=f'bar{i}', elem_classes='bar_button'))
+                            bar_dropdown[i] = gr.Dropdown(show_label=False, choices=['self','preset1','preset2','preset3'], value='self')
 
                 with gr.Row():
                     progress_window = grh.Image(label='Preview', show_label=False, visible=True, height=768, elem_id='preview_generating',
