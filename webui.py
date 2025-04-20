a9fb2b0ac57fd611b4a31cdb3bdb812bb303bb9f
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-04-19 23:41:36 -0700

    Update webui.py Reduced bar button min width to 70
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- webui.py
+++ webui.py
@@ -211,7 +211,7 @@ with common.GRADIO_ROOT:
                         bar_buttons = []
                         category_list = ['Favorites', 'Alternative', 'Fantasy', 'Flux1D', 'Flux1S', 'LowVRAM', 'Pony', 'Realism', 'SD1.5']
                         for i in range(len(category_list)):
-                            bar_buttons.append(gr.Button(value=category_list[i], size='sm', visible=True, min_width=90, elem_id=f'bar{i}', elem_classes='bar_button'))
+                            bar_buttons.append(gr.Button(value=category_list[i], size='sm', visible=True, min_width=70, elem_id=f'bar{i}', elem_classes='bar_button'))
                         #bar_dropdown = gr.Dropdown(show_label=False, choices=['self','preset1','preset2','preset3'], value='self')
 
                 with gr.Row():
