8541e17ce685fdd2936c677c7ca5b4d6afacddf4
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-04-22 18:09:09 -0700

    Update webui.py Rename try_get_preset_content to get_preset_content
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- webui.py
+++ webui.py
@@ -208,10 +208,10 @@ with common.GRADIO_ROOT:
                         # disable the iFrame display of help for preset selections:
                         preset_instruction = gr.HTML(visible=False, value=topbar.preset_no_instruction())
                         
-                        bar_title = gr.Markdown('<b>Presets:</b>', visible=True, elem_id='bar_title', elem_classes='bar_title')
+                        bar_title = gr.Markdown('<b>Presets:</b>', visible=False, elem_id='bar_title', elem_classes='bar_title')
                         bar_buttons = []
                         for i in range(topbar.topbar_limit):
-                            bar_buttons.append(gr.Button(value='Default' if i==0 else '', size='sm', visible=True, min_width=90, elem_id=f'bar{i}', elem_classes='bar_button'))
+                            bar_buttons.append(gr.Button(value='Default' if i==0 else '', size='sm', visible=False, min_width=90, elem_id=f'bar{i}', elem_classes='bar_button'))
                         #bar_dropdown = gr.Dropdown(show_label=False, choices=['self','preset1','preset2','preset3'], value='self')
 
                 with gr.Row():
@@ -1187,7 +1187,7 @@ with common.GRADIO_ROOT:
 
         if not args_manager.args.disable_preset_selection:
             def _change(preset, is_generating, inpaint_mode):
-                preset_content = modules.config.try_get_preset_content(preset) if preset != 'initial' else {}
+                preset_content = PR.get_preset_content(preset) if preset != 'initial' else {}
                 preset_prepared = modules.meta_parser.parse_meta_from_preset(preset_content)
 
                 default_model = preset_prepared.get('base_model')
