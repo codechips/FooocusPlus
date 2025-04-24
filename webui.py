c4a1b914023737925ca308ea6e38620ae0f67179
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-04-23 19:49:46 -0700

    Update webui.py Now category_selection.change uses PR.category_selection()
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- webui.py
+++ webui.py
@@ -679,7 +679,7 @@ with common.GRADIO_ROOT:
                             value=args_manager.args.preset if args_manager.args.preset else "initial",
                             visible=True, interactive=True)
 
-                        category_selection.change(PR.category_selection=category_selection, inputs=category_selection,
+                        category_selection.change(PR.category_selection(category_selection), inputs=category_selection,
                             outputs=preset_selection, show_progress=False, queue=False)
                         print(f'Category_selection: {category_selection}')
                         print(f'PR.Category_selection: {pr.category_selection}')
