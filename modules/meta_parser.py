e0ed3dcdc06c565d25e33fd4834bedb278d0ce68
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-04-25 17:07:44 -0700

    Update meta_parser.py Try default_aspect_ratio = CURRENT_ASPECT
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- modules/meta_parser.py
+++ modules/meta_parser.py
@@ -395,7 +395,9 @@ def parse_meta_from_preset(preset_content):
                 default_aspect_ratio = items[settings_key]
                 width, height = default_aspect_ratio.split('*')
             else:
-                default_aspect_ratio = getattr(modules.config, settings_key)
+#                default_aspect_ratio = getattr(modules.config, settings_key)
+                default_aspect_ratio = common.CURRENT_ASPECT
+                print(f'CURRENT_ASPECT in meta_parser: {CURRENT_ASPECT}')
                 width, height = default_aspect_ratio.split('×')
                 height = height[:height.index(" ")]
             preset_prepared[meta_key] = (width, height)
