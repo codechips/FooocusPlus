8cf62f16e771a1853d917a9ca5b7b5181e57bc64
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-04-26 16:06:09 -0700

    Update topbar.py Add dummy string to replace welcome image
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- enhanced/topbar.py
+++ enhanced/topbar.py
@@ -213,7 +213,8 @@ def init_nav_bars(state_params, request: gr.Request):
     state_params.update({"bar_button": PR.current_preset})
     state_params.update({"init_process": 'finished'})
     results = refresh_nav_bars(state_params)
-#    results += [gr.update(value=f'{file_welcome}')]    
+#    results += [gr.update(value=f'{file_welcome}')]
+    results += [gr.update(value='')]  # add dummy value
     results += [gr.update(value=modules.flags.language_radio(state_params["__lang"])), gr.update(value=state_params["__theme"])]
     results += [gr.update(choices=state_params["__output_list"], value=None), gr.update(visible=len(state_params["__output_list"])>0, open=False)]
     results += [gr.update(value=False if state_params["__is_mobile"] else config.default_inpaint_advanced_masking_checkbox)]
