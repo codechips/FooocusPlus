591d3c3938bc2ec4789bb52addba01c1297b1022
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-04-26 22:13:12 -0700

    Update common.py Added POSITIVE and NEGATIVE pseudo globals
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- common.py
+++ common.py
@@ -10,6 +10,11 @@ CURRENT_ASPECT = ''
 # Store the aspect ratio selection from the current preset
 AR_TEMPLATE = 'Std.'
 
+# Store prompts from async_worker
+# used to restore prompts after preset change clears them
+POSITIVE = ''
+NEGATIVE = ''
+
 # ROOT is used as a constant that
 # is referenced by several modules
 ROOT = os.path.dirname(os.path.abspath(__file__))
