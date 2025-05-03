40887c18c7cabb293d4292889b6dbc26629db15b
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-05-03 12:24:10 -0700

    Update ui_support.py Now check_absent_model() is called bar_button_change()
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- modules/ui_support.py
+++ modules/ui_support.py
@@ -276,9 +276,10 @@ def sync_message(state_params):
     return state_params
 
 preset_down_note_info = 'Loading model files...'
-def check_absent_model(bar_button, state_params):
+def bar_button_change(bar_button, state_params):
     #print(f'check_absent_model,state_params:{state_params}')
     state_params.update({'bar_button': bar_button})
+    refresh_topbar_status_js
     return state_params
 
 def down_absent_model(state_params):
