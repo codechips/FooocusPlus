d8a6b4070bed8c43462ab72c0963ce0246286ec5
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-05-02 20:04:00 -0700

    Update webui.py gr Syntax
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- webui.py
+++ webui.py
@@ -305,7 +305,7 @@ with common.GRADIO_ROOT:
                         input_image_checkbox = gr.Checkbox(label='Input Image', value=modules.config.default_image_prompt_checkbox, container=False, elem_classes='min_check')              
                         prompt_panel_checkbox = gr.Checkbox(label='Wildcard Panel', value=False, container=False, elem_classes='min_check')
                         advanced_checkbox = gr.Checkbox(label='Advanced', value=modules.config.default_advanced_checkbox, container=False, elem_classes='min_check')
-                with gr.column():
+                with gr.Column():
                     preset_info = gr.Markdown(value=f'<b>Current Preset: {PR.current_preset}</b>', container=False, visible=True)
             
             with gr.Group(visible=False, elem_classes='toolbox') as image_toolbox:
