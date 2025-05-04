92d7674858756236bdef4ace3bbf3d09ac14da1b
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-05-04 12:41:23 -0700

    Update ui_support.py Attempt to save positive & negative prompts
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- modules/ui_support.py
+++ modules/ui_support.py
@@ -258,8 +258,6 @@ def process_after_generation(state_params):
     # gallery_index, index_radio
     results += [gr.update(choices=state_params["__output_list"], value=None), gr.update(visible=len(state_params["__output_list"])>0, open=False)]
     # prompt, random_button, translator_button, super_prompter, background_theme, image_tools_checkbox, bar0_button, bar1_button, bar2_button, bar3_button, bar4_button, bar5_button, bar6_button, bar7_button, bar8_button
-# REM the next line to avoid errors
-#    preset_nums = len(state_params["__nav_name_list"].split(','))
     preset_nums = PR.preset_count()
     results += [gr.update(interactive=True)] * (preset_nums + 6)
     results += [gr.update()] * (preset_nums)
@@ -403,10 +401,14 @@ def prompt_token_prediction(text, style_selections):
     prompts = remove_empty_str([safe_str(p) for p in prompt.splitlines()], default='')
 
     prompt = prompts[0]
-
     if prompt == '':
         # disable expansion when empty since it is not meaningful and influences image prompt
         use_expansion = False
+    else:
+        print(f'UIS Prompt: {prompt}')
+        common.POSITIVE = prompt
+    if negative_prompt != '':
+        common.NEGATIVE = negative_prompt
 
     extra_positive_prompts = prompts[1:] if len(prompts) > 1 else []
     task_rng = random.Random(random.randint(constants.MIN_SEED, constants.MAX_SEED))
