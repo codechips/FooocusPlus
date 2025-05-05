3f0d3e2dd78b3e5b24f4981e8392d53f7020a7af
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-05-05 13:27:54 -0700

    Update webui.py Removed state_topbar
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- webui.py
+++ webui.py
@@ -1553,8 +1553,8 @@ with common.GRADIO_ROOT:
            .then(inpaint_engine_state_change, inputs=[inpaint_engine_state] + enhance_inpaint_mode_ctrls,\
                outputs=enhance_inpaint_engine_ctrls, queue=False, show_progress=False)
 
-        preset_selection.change(PR.set_preset_selection, inputs=[preset_selection, state_topbar], \
-            outputs=[preset_selection, state_topbar, preset_info], show_progress=False, queue=False) \
+        preset_selection.change(PR.set_preset_selection, inputs=preset_selection, \
+            outputs=[preset_selection, preset_info], show_progress=False, queue=False) \
            .then(UIS.reset_layout_params, inputs=reset_preset_inputs, outputs=reset_layout_params, show_progress=False) \
            .then(fn=lambda x: x, inputs=state_topbar, outputs=system_params, show_progress=False) \
            .then(fn=lambda x: {}, inputs=system_params, outputs=system_params, _js=UIS.refresh_topbar_status_js) \
