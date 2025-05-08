61a480757e4d6c3bf5617698c8a15aad5feee49a
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-05-07 17:14:26 -0700

    Update config.py Update AR.current_AR with the default_standard_aspect_ratio
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- modules/config.py
+++ modules/config.py
@@ -1225,6 +1225,7 @@ update_files()
 
 
 # Additional aspect ratio support
+AR.current_AR = default_standard_aspect_ratio
 
 default_aspect_ratio_values = [default_standard_aspect_ratio, default_shortlist_aspect_ratio,\
     default_sd1_5_aspect_ratio, default_pixart_aspect_ratio,]
