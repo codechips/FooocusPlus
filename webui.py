44f5c25d0bf8b036ee9462671712e22a9f08fef2
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-05-09 18:21:06 -0700

    Update webui.py Add extra return items to AR routines
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- webui.py
+++ webui.py
@@ -715,8 +715,10 @@ with common.GRADIO_ROOT:
                             elem_classes='aspect_ratios'))
 
                         for aspect_ratios_select in aspect_ratios_selections:
-                            aspect_ratios_select.change(AR.save_current_aspect, inputs=aspect_ratios_select, outputs=aspect_ratios_selection,\
-                                queue=False, show_progress=False).then(lambda x: None, inputs=aspect_ratios_select, queue=False,\
+                            aspect_ratios_select.change(AR.save_current_aspect, inputs=aspect_ratios_select,\
+                                outputs=[aspect_ratios_selection, aspect_ratios_selection],\
+                                queue=False, show_progress=False)\
+                                .then(lambda x: None, inputs=aspect_ratios_select, queue=False,\
                                 show_progress=False, _js='(x)=>{refresh_aspect_ratios_label(x);}')
                         
                         overwrite_width = gr.Slider(label='Forced Overwrite of Generating Width',
@@ -1310,7 +1312,8 @@ with common.GRADIO_ROOT:
             
         aspect_ratios_selection.change(AR.reset_aspect_ratios , inputs=aspect_ratios_selection,\
             outputs=aspect_ratios_selections, queue=False, show_progress=False)\
-            .then(AR.save_AR_template, inputs=aspect_ratios_selection, outputs=aspect_ratios_selection, queue=False,\
+            .then(AR.save_AR_template, inputs=aspect_ratios_selection,\
+            outputs=[aspect_ratios_selection, aspect_ratios_selection], queue=False,\
             show_progress=False, _js='(x)=>{refresh_aspect_ratios_label(x);}')
 
         output_format.input(lambda x: gr.update(output_format=x), inputs=output_format)
