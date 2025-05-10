8b07271d038f5063ac239a0e5f0f63e6e7487b4a
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-05-09 17:24:52 -0700

    Update webui.py Syntax (missing comma)
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- webui.py
+++ webui.py
@@ -1309,7 +1309,7 @@ with common.GRADIO_ROOT:
                                      ], queue=False, show_progress=False)     
             
         aspect_ratios_selection.change(AR.reset_aspect_ratios , inputs=aspect_ratios_selection,\
-            outputs=[aspect_ratios_selections, aspect_ratios_selection]\
+            outputs=[aspect_ratios_selections, aspect_ratios_selection],\
             queue=False, show_progress=False).then(lambda x: None, inputs=aspect_ratios_selection, queue=False,\
             show_progress=False, _js='(x)=>{refresh_aspect_ratios_label(x);}')
 
