c35c69b02ed977f2e6390e858bc7a0cebeb3dd22
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-05-11 12:34:09 -0700

    Update config.py Now sets AR.shortlist_default to Shortlist default AR value
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- modules/config.py
+++ modules/config.py
@@ -1237,8 +1237,15 @@ update_files()
 
 
 # Additional aspect ratio support
+
+def assign_default_by_template(template):
+    ar_index = AR.aspect_ratios_templates.index(template)
+    return default_aspect_ratio_values[ar_index]
+
+# initialize AR globals
 AR.current_AR = default_standard_aspect_ratio
 AR.AR_shortlist = enable_shortlist_aspect_ratios
+AR.shortlist_default = modules.config.assign_default_by_template('Shortlist')
 
 default_aspect_ratio_values = [default_standard_aspect_ratio, default_shortlist_aspect_ratio,\
     default_sd1_5_aspect_ratio, default_pixart_aspect_ratio,]
@@ -1246,8 +1253,4 @@ config_aspect_ratio_title = AR.aspect_ratio_title(default_aspect_ratio_values)
 
 config_aspect_ratios = [available_standard_aspect_ratios, available_shortlist_aspect_ratios,\
     available_sd1_5_aspect_ratios, available_pixart_aspect_ratios,]
-config_aspect_ratio_labels = AR.aspect_ratio_labels(config_aspect_ratios)
-
-def assign_default_by_template(template):
-    ar_index = AR.aspect_ratios_templates.index(template)
-    return default_aspect_ratio_values[ar_index]
+config_aspect_ratio_labels = AR.aspect_ratio_labels(config_aspect_ratios) 
