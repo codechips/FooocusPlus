260289236f0692a10d16a9f81a84753273f13978
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-05-10 20:57:02 -0700

    Update aspect_ratios.py Renable shortlist toggle in reset_aspect_ratios()
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- modules/aspect_ratios.py
+++ modules/aspect_ratios.py
@@ -105,10 +105,10 @@ def reset_aspect_ratios(arg_AR):
     aspect_ratios = arg_AR.split(',')[0]
     if aspect_ratios:
         current_AR = aspect_ratios
-#    if (AR_shortlist == True) and (AR_template == 'Standard'):
-#        AR_template = 'Shortlist'
-#    elif (AR_shortlist == False) and (AR_template == 'Shortlist'):
-#        AR_template = 'Standard'
+    if (AR_shortlist == True) and (AR_template == 'Standard'):
+        AR_template = 'Shortlist'
+    elif (AR_shortlist == False) and (AR_template == 'Shortlist'):
+        AR_template = 'Standard'
     if AR_template == 'Shortlist':
         results = [gr.update(visible=False), gr.update(value=aspect_ratios, visible=True)] + [gr.update(visible=False)] * 2
     elif AR_template=='SD1.5':
