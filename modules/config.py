ced96df582503e055035ceb6cf1ed2f881887462
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-05-14 10:50:25 -0700

    Update config.py Removed the Shortlist/Standard icon setup
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- modules/config.py
+++ modules/config.py
@@ -1237,8 +1237,6 @@ update_files()
 
 
 # Additional aspect ratio support
-available_standard_aspect_ratios.append(AR.shortlist_icon)
-available_shortlist_aspect_ratios.append(AR.standard_icon)
 
 config_aspect_ratios = [available_standard_aspect_ratios, available_shortlist_aspect_ratios,\
     available_sd1_5_aspect_ratios, available_pixart_aspect_ratios,]
