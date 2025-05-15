aa689b3fce888a376b02f0362385ada5e7e85344
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-05-15 15:58:03 -0700

    Update preset_resource.py Removed set_category_selection() output to preset_selection
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- modules/preset_resource.py
+++ modules/preset_resource.py
@@ -107,8 +107,7 @@ def set_category_selection(arg_category_selection):
             preset_value = preset_choices[0]
     return gr.update(value=category_selection),\
         gr.update(choices=preset_choices, value=preset_value),\
-        gr.update(value=f'Current Preset: {current_preset}'),\
-        gr.update(value=AR.current_AR)
+        gr.update(value=f'Current Preset: {current_preset}')
 
 def set_preset_selection(arg_preset_selection, state_params):
     global current_preset
