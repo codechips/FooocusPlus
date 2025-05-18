6a72e70dade27f66ef365f18517cc71801b4e196
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-05-17 17:47:44 -0700

    Update meta_parser.py Removed the unnecessary msgs about "engines"
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- modules/meta_parser.py
+++ modules/meta_parser.py
@@ -224,11 +224,9 @@ def get_resolution(key: str, fallback: str | None, source_dict: dict, results: l
             template = source_dict['engine'].get('available_aspect_ratios_selection',\
                 default_class_params[engine].get('available_aspect_ratios_selection',\
                 default_class_params['Fooocus']['available_aspect_ratios_selection']))
-            print(f'Defaulting to the {template} template, with reference to the {engine} engine')
         else:
             template = default_class_params[engine].get('available_aspect_ratios_selection',\
                 default_class_params['Fooocus']['available_aspect_ratios_selection'])
-            print(f'Defaulting to the {template} template, without reference to the {engine} engine')
 
         if 'SD1.5' in str(AR.preset_file) and template!='SD1.5':
             template = 'SD1.5'
