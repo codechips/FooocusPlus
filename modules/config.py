5e72bf4d8b05aed0187c75d4b8385f0b81f4e2a0
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-05-19 18:33:32 -0700

    Update config.py Test print all_presetnames
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- modules/config.py
+++ modules/config.py
@@ -241,10 +241,12 @@ enable_preset_bar = get_config_item_or_set_default(
     validator=lambda x: isinstance(x, bool),
     expected_type=bool
 )
+all_presetnames = PR.get_all_presetnames()
+print(f'all_presetnames {all_presetnames}')
 preset_bar_category = get_config_item_or_set_default(
     key='preset_bar_category',
     default_value='Favorite',
-    validator=lambda x: x in PR.get_all_presetnames(),
+    validator=lambda x: x in all_presetnames(),
     expected_type=str
 )
 preset_bar_length = get_config_item_or_set_default(
