cb2e2c71a2f529176a16170ab7d9d99cff4a4ca0
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-05-21 23:35:56 -0700

    Update webui.py Converted them back to dropdowns
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- webui.py
+++ webui.py
@@ -970,11 +970,11 @@ with common.GRADIO_ROOT:
                 with gr.Column(visible=modules.config.default_expert_mode_checkbox) as dev_tools:
                     with gr.Tab(label='Expert Tools'):
                         
-                        sampler_name = gr.Accordion(label='Sampler', choices=flags.sampler_list, \
+                        sampler_name = gr.Dropdown(label='Sampler', choices=flags.sampler_list, \
                             value=modules.config.default_sampler, interactive=True, visible=True, css="color: white; font-weight: bold", elem_classes='sampler_name')
-                        scheduler_name = gr.Accordion(label='Scheduler', choices=flags.scheduler_list, \
+                        scheduler_name = gr.Dropdown(label='Scheduler', choices=flags.scheduler_list, \
                             value=modules.config.default_scheduler, css="color: white; font-weight: bold")
-                        vae_name = gr.Accordion(label='VAE', choices=[modules.flags.default_vae] + modules.config.vae_filenames, \
+                        vae_name = gr.Dropdown(label='VAE', choices=[modules.flags.default_vae] + modules.config.vae_filenames, \
                             value=modules.config.default_vae, show_label=True)                         
 
                         clip_skip = gr.Slider(label='CLIP Skip', minimum=1, maximum=flags.clip_skip_max, step=1, \
