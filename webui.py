3a0ee89eb951e1e1419ca120cfd85e4782ac9f07
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-05-22 12:51:29 -0700

    Update webui.py Restored favorites_checkbox to load_data_outputs
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- webui.py
+++ webui.py
@@ -1278,11 +1278,11 @@ with common.GRADIO_ROOT:
         state_is_generating = gr.State(False)
         
         #substituted preset_bar_checkbox for advanced_checkbox to avoid toggling Advanced when preset_bar activated
-        load_data_outputs = [image_number, prompt, negative_prompt, style_selections,
+        load_data_outputs = [favorites_checkbox, image_number, prompt, negative_prompt, style_selections,
                  performance_selection, overwrite_step, overwrite_switch, aspect_ratios_selection,
                  overwrite_width, overwrite_height, guidance_scale, sharpness, adm_scaler_positive,
                  adm_scaler_negative, adm_scaler_end, refiner_swap_method, adaptive_cfg,
-                 clip_skip, base_model, refiner_model, refiner_switch, secret_name, sampler_name,
+                 clip_skip, base_model, refiner_model, refiner_switch, secret_name, 
                  scheduler_name, vae_name, seed_random, image_seed, inpaint_engine,
                  inpaint_engine_state, inpaint_mode] + enhance_inpaint_mode_ctrls + [generate_button,
                  load_parameter_button] + freeu_ctrls + lora_ctrls
