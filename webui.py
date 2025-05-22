c8b20ead22d3824f81d710046d32520e6b246779
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-05-22 11:39:59 -0700

    Update webui.py Put language_ui in load_data_outputs
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- webui.py
+++ webui.py
@@ -1281,7 +1281,7 @@ with common.GRADIO_ROOT:
                  performance_selection, overwrite_step, overwrite_switch, aspect_ratios_selection,
                  overwrite_width, overwrite_height, guidance_scale, sharpness, adm_scaler_positive,
                  adm_scaler_negative, adm_scaler_end, refiner_swap_method, adaptive_cfg,
-                 clip_skip, base_model, refiner_model, refiner_switch, sampler_name, scheduler_name,
+                 clip_skip, base_model, refiner_model, refiner_switch, language_ui, sampler_name, scheduler_name,
                  vae_name, seed_random, image_seed, inpaint_engine, inpaint_engine_state,
                  inpaint_mode] + enhance_inpaint_mode_ctrls + [generate_button,
                  load_parameter_button] + freeu_ctrls + lora_ctrls
