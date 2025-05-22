83d6e2f1aa03a309b06897451d3c51bad68bd6fb
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-05-22 12:01:22 -0700

    Update ui_support.py More test prints
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- modules/ui_support.py
+++ modules/ui_support.py
@@ -324,9 +324,11 @@ def reset_layout_params(prompt, negative_prompt, state_params, is_generating, in
     state_params.update({"__preset_url":preset_url})
     print('Prior to refresh_nav_bars')
     results = refresh_nav_bars(state_params)
+    print('After to refresh_nav_bars')
     results += meta_parser.switch_layout_template(preset_prepared, state_params, preset_url)
+    print('After to refresh_nav_bars 2')
     results += meta_parser.load_parameter_button_click(preset_prepared, is_generating, inpaint_mode)
-
+    print('After to refresh_nav_bars 3')
     return results
 
 
