1631dc918c0f689719219acea3f54a169373e482
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-05-24 12:50:34 -0700

    Update version.py Moved python_embedded_path & is_win32_standalone_build here from launch.support
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- enhanced/version.py
+++ enhanced/version.py
@@ -1,7 +1,10 @@
 import os
 import sys
 from pathlib import Path
-from launch_support import is_win32_standalone_build
+
+win32_root = os.path.dirname(os.path.dirname(__file__))
+python_embedded_path = os.path.join(win32_root, 'python_embedded')
+is_win32_standalone_build = os.path.exists(python_embedded_path) and os.path.isdir(python_embedded_path)
 
 commit_id = ''
 fooocusplus_ver = ''
