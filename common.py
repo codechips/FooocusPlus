edeee950f9c8c8b4c9514c5d79ba19f9209bb973
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-05-24 19:28:35 -0700

    Update common.py Added torch_installed, to check if this is a new installation
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- common.py
+++ common.py
@@ -5,11 +5,14 @@ import os
 GRADIO_ROOT = None
 MODELS_INFO = None
 
-# Store prompts from async_worker
-# used to restore prompts after preset change clears them
+# Store the current prompts
+# used to restore prompts after a preset change clears them
 POSITIVE = ''
 NEGATIVE = ''
 
 # ROOT is used as a constant that
 # is referenced by several modules
 ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# flags whether or not this is a new installation
+torch_installed = false
