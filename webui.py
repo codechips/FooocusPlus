e91fcc732abf4b6d74b78595caca64173bb62c7f
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-05-28 23:06:32 -0700

    Update webui.py Repair an improper ref to modules.config at L1108
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- webui.py
+++ webui.py
@@ -1105,7 +1105,7 @@ with common.GRADIO_ROOT:
                     print()
                     print('Refreshing all files...')
                     modules.user_structure.create_user_structure(modules.config.user_dir)
-                    modules.user_structure.create_model_structure(modules.config.paths_checkpoints, config.paths_loras)
+                    modules.user_structure.create_model_structure(modules.config.paths_checkpoints, modules.config.paths_loras)
                     engine = state_params.get('engine', 'Fooocus')
                     task_method = state_params.get('task_method', None)
                     model_filenames, lora_filenames, vae_filenames = modules.config.update_files(engine, task_method)
